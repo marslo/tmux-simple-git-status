1a460944056da160bcd417cddb2ed7f57be442ee
marslo <marslo@marvell.com>
2025-01-17 04:48:48 -0800

    remove the leading and tralling space
    Signed-off-by: marslo <marslo@marvell.com>

--- scripts/git_status.sh
+++ scripts/git_status.sh
@@ -1,44 +1,32 @@
 #!/usr/bin/env bash
 
+# shellcheck disable=SC2155
+
 PANE_PATH=$(tmux display-message -p -F "#{pane_current_path}")
-cd $PANE_PATH
+cd "${PANE_PATH}" || return
 
 git_changes() {
   local changes=$(git diff --shortstat | sed 's/^[^0-9]*\([0-9]*\)[^0-9]*\([0-9]*\)[^0-9]*\([0-9]*\)[^0-9]*/\1;\2;\3/')
-  local changes_array=(${changes//;/ })
+  local changes_array=("${changes//;/ }")
   local untracked=$(git status --porcelain 2>/dev/null| grep -c "^??")
   local result=()
-  
-  if [[ $untracked != 0 ]]; then
-    result+=("?$untracked")
-  fi
-  if [[ -n ${changes_array[0]} ]]; then
-    result+=("~${changes_array[0]}")
-  fi
 
-  if [[ -n ${changes_array[1]} ]]; then
-    result+=("+${changes_array[1]}")
-  fi
-
-  if [[ -n ${changes_array[2]} ]]; then
-    result+=("-${changes_array[2]}")
-  fi
+  [[ $untracked != 0 ]] && result+=("?$untracked")
+  [[ -n ${changes_array[0]} ]] && result+=("~${changes_array[0]}")
+  [[ -n ${changes_array[1]} ]] && result+=("+${changes_array[1]}")
+  [[ -n ${changes_array[2]} ]] && result+=("-${changes_array[2]}")
 
   local joined=$(printf " %s" "${result[@]}")
   local joined=${joined:1}
 
-  if [[ -n $joined ]]; then
-    echo "$joined "
-  fi
+  [[ -n $joined ]] && echo "$joined"
 }
 
 git_status() {
   local status=$(git rev-parse --abbrev-ref HEAD)
   local changes=$(git_changes)
 
-  if [[ -n $status ]]; then
-    printf " $status $changes"
-  fi
+  [[ -n $status ]] && printf "%s %s" "${status}" "${changes}"
 }
 
 main() {
